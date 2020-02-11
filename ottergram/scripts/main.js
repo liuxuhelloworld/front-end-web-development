c099e2b3a6af7875eec10e6349cfb1ec95f7f29c
liuxuhelloworld <liuxuhelloworld@126.com>
2020-02-11 09:04:22 +0800

    simple JS example

---
+++ ottergram/scripts/main.js
@@ -0,0 +1,58 @@
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+
+function setDetails(imageUrl, titleText) {
+    'use strict';
+
+    var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumbnail) {
+    'use strict';
+
+    thumbnail.addEventListener('click', function (event) {
+        event.preventDefault();
+        setDetailsFromThumb(thumbnail);
+    });
+}
+
+function getThumbnailsArray() {
+    'use strict';
+
+    var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var thumbnailsArray = [].slice.call(thumbnails);
+
+    return thumbnailsArray;
+}
+
+function initializeEvents() {
+    'use strict';
+
+    var thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
